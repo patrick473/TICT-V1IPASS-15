9ed029c6dc0a23a6128ba475d08f8266f22e0c88
patrick <patkottman@gmail.com>
2017-06-04 12:01:49 +0200

    dao
    rubriekdao af

--- src/main/java/dao/RubriekDAO.java
+++ src/main/java/dao/RubriekDAO.java
@@ -1,5 +1,45 @@
 package dao;
 
-public class RubriekDAO {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
+import model.Rubriek;
+
+public class RubriekDAO extends BaseDAO{
+		private ArrayList<Rubriek> selectRubrieken(String query){
+			ArrayList<Rubriek> rubrieklijst = new ArrayList<Rubriek>();
+			try(Connection con = super.connect()){
+				Statement stmt = con.createStatement();
+		ResultSet dbResultSet = stmt.executeQuery(query);
+		
+		while (dbResultSet.next()){
+			int rubriekNummer = dbResultSet.getInt("rubrieknummer");
+			String rubriekNaam = dbResultSet.getString("rubrieknaam");
+			int inRubriek = dbResultSet.getInt("inrubriek");
+			Rubriek rubriek= new Rubriek(rubriekNummer,rubriekNaam,inRubriek);
+			rubrieklijst.add(rubriek);
+		}
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+		}
+			return rubrieklijst;
+	}
+
+	public ArrayList<Rubriek> selectAll() {
+		return selectRubrieken("SELECT * FROM bod");
+	}
+
+	public Rubriek findByCode(int id) {
+		return selectRubrieken("SELECT * FROM bod WHERE rubrieknummer = " + id + "").get(0);
+	}
+	public ArrayList<Rubriek> selectBovensteRubriek(){
+		return selectRubrieken("select * from rubriek where inrubriek is null");
+	}
+	public ArrayList<Rubriek> selectBovensteRubriek(int id){
+		return selectRubrieken("select * from rubriek where inrubriek ="+id+"");
+	}
 }
