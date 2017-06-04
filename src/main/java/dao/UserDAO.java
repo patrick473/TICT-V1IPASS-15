fb9bb9371121c2c0d6ae53ecb2cfdab1ce048204
patrick <patkottman@gmail.com>
2017-06-04 19:55:46 +0200

    authentication added

---
+++ src/main/java/dao/UserDAO.java
@@ -0,0 +1,37 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class UserDAO extends BaseDAO {
+	 public String findRoleForUsernameAndPassword(String username, String password) {
+	 String role = null;
+	 String query = "SELECT kanverkopen FROM gebruiker WHERE gebruikersnaam = ? AND wachtwoord = ?";
+
+	 try (Connection con = super.connect()) {
+
+	 PreparedStatement pstmt = con.prepareStatement(query);
+	 pstmt.setString(1, username);
+	 pstmt.setString(2, password);
+
+	 ResultSet rs = pstmt.executeQuery();
+	 if (rs.next()){
+		 int kanverkopen = rs.getInt("kanverkopen");
+		 if (kanverkopen == 1){
+			 role = "verkoper";
+		 }
+		 else{
+			 role = "gebruiker";
+		 }
+	
+	 
+	 }
+	 } catch (SQLException sqle) {
+	 sqle.printStackTrace();
+	 }
+
+	 return role;
+	 }
+	}
