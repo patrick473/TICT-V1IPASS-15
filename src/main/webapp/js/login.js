f52fd80324f353861dde8874528993cab1da33b0
patrick <patkottman@gmail.com>
2017-06-11 20:52:17 +0200

    every second refresh login causes 412  error

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -12,3 +12,33 @@ $("input, textarea").alphanum({
     forceLower         : false,
     maxLength          : NaN
 });
+$("#loginForm").validate({
+       rules: {
+           gebruikersnaam: {
+               required: true,
+           },
+           password: {
+               required: true,
+           },
+
+       },
+       //For custom messages
+       messages: {
+           gebruikersnaam:{
+               required: "voer een gebruikersnaam in.",
+
+           },
+           password:{
+               required: "voer een wachtwoord in."
+           }
+       },
+       errorElement : 'div',
+       errorPlacement: function(error, element) {
+         var placement = $(element).data('error');
+         if (placement) {
+           $(placement).append(error)
+         } else {
+           error.insertAfter(element);
+         }
+       }
+    });
