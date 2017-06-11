dd0335a6019dd9bbd07253a41804b6bc1143db49
patrick <patkottman@gmail.com>
2017-06-11 20:34:19 +0200

    :two: extra dependencies

---
+++ src/main/webapp/js/login.js
@@ -0,0 +1,14 @@
+$("input, textarea").alphanum({
+    allow              : '!@#$%&*(){}|:.,<>~+=_-?/',
+    disallow           : '',
+    allowSpace         : false,
+    allowNumeric       : true,
+    allowUpper         : true,
+    allowLower         : true,
+    allowCaseless      : true,
+    allowLatin         : true,
+    allowOtherCharSets : true,
+    forceUpper         : false,
+    forceLower         : false,
+    maxLength          : NaN
+});
