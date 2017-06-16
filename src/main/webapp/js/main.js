ac121fc7ddee0138e15461fb44a554f26fc67317
patrick <patkottman@gmail.com>
2017-06-16 15:16:34 +0200

    made css more responsive like

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
     $(".sideNavDropOut").sideNav();
 $(".username").html(sessionStorage.getItem("gebruikersnaam"));
+var v = (localStorage.getItem('checkcalfid')=='true');
+if(v){
+    console.log("gebruiker kan verkopen");
+}
+else {
+    console.log("gebruiker kan niet verkopen");
+    $(".verkoperItem").addClass('hide');
+}
 });
